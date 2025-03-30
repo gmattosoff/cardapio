c6d24499fc294266606a06f1edea1a263b1a1bd3
Gustavo Mattos da Silva <130101081+gmattosoff@users.noreply.github.com>
2025-03-30 14:54:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const breakfasts = Array.from(document.getElementsByClassName("Breakfast"));
+const lunches = Array.from(document.getElementsByClassName("Lunch"));
+const shakes = Array.from(document.getElementsByClassName("Shakes"));
+const dinners = Array.from(document.getElementsByClassName("Dinner"));
+const all = [...breakfasts, ...lunches, ...shakes, ...dinners];
+const lis = Array.from(document.querySelectorAll("li"));
+const c1 = document.getElementById("column1")
+
+lis.forEach((li) => {
+    li.addEventListener("click", function() {
+        for (var i=0; i < all.length; i++) {
+            if (li.innerHTML == all[i].className || li.innerHTML == "All") {
+                all[i].style.display = "flex";
+            } else {
+                all[i].style.display = "none";
+            };
+        };
+    });
+});
